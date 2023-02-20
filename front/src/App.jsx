80da44e44bddf62ab4da43594961ad215bed202e
Joshua Cooper <65969149+joshcooper6@users.noreply.github.com>
2023-02-20 13:25:05 -0800

    server restructuring

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -15,17 +15,6 @@ function App() {
   const [vinyls, setVinyls] = useState([]);
   const [cart, setCart] = useState([]);
   
-  // const { data2, status } = useQuery('vinyls', async () => {
-  //   const response = await fetch('http://localhost:2222/vinyls');
-  //   return response.json();
-  // });
-  
-  // useEffect(() => {
-  //   vinyls.map((vinyl, indx) => {
-  //     return console.log(vinyl)
-  //   });
-  // }, []);
-
   useEffect(() => { if (cart.length > 0) { setShowCart(true) } }, [cart]);
 
   return (
@@ -33,35 +22,7 @@ function App() {
       
       <Header showCart={showCart} setShowCart={setShowCart} />
 
-      {/* <div className='w-11/12 bg-blue-100 drop-shadow-md rounded-lg mt-4 mb-3 h-[200px] flex flex-col justify-center items-center'>
-        promotional content will go here
-      </div> */}
-
       <div className='flex flex-col w-screen'>
-        {/* <div className='mb-6'>
-          <h2
-            className='p-6 text-5xl font-bold lowercase tracking-tight'
-            children={'featured albums'}
-          />
-        </div> */}
-
-          {/* <div className='flex w-full mb-6 max-w-[1200px] gap-6 self-center justify-center items-center flex-wrap'>
-            { vinyls.map((vinyl, indx) => {
-              return <AlbumCard 
-                vinyl={vinyl} 
-                setVinyls={setVinyls}
-                cart={cart}
-                setCart={setCart}
-              />
-            }) }
-          </div> */}
-
-          <Inventory
-            // vinyls={vinyls}
-            // setVinyls={setVinyls}
-            cart={cart}
-            setCart={setCart}
-          />
 
       </div>
 
@@ -71,12 +32,7 @@ function App() {
 
       <SideCart cart={cart} setCart={setCart} setShowCart={setShowCart} showCart={showCart} />
       
-      <Footer />
-      {/* <button 
-        onClick={() => setShowCart(prev => !prev)} 
-        className={`fixed right-0 z-[200] text-black top-0 p-4 transease`}
-        children={showCart ? 'X' : <img src={tryThisCart} />}
-      /> */}
+      {/* <Footer /> */}
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,152 +1,189 @@
-require('dotenv').config();
-const { Converter } = require('aws-sdk/clients/dynamodb');
-const stripe = require('stripe')(process.env.STRIPE_SEC);
-const express = require('express');
-const data = require('./data');
+require("dotenv").config();
+const { Converter } = require("aws-sdk/clients/dynamodb");
+const stripe = require("stripe")(process.env.STRIPE_SEC);
+const express = require("express");
+const data = require("./data");
 const app = express();
 const port = 2222;
-const { transformArray } = require('./functions/transformArray');
-const database = require('./configuredb.js').db;
+const { transformArray } = require("./functions/transformArray");
+const database = require("./configuredb.js").db;
 
 app.use(express.json());
-app.use(require('cors')());
+app.use(require("cors")());
 app.use(express.urlencoded({ extended: true }));
 
 const DOMAIN = `http://127.0.0.1:5173/`;
 
-app.get('/vinyls', (req, res) => {
-    res.send(data.data);
+async function getProducts() {
+  return await stripe.products.list({ limit: 100 });
+}
+
+async function getPrice(priceId) {
+  return await stripe.prices.retrieve(priceId)
+}
+
+async function getPrices() {
+  return await stripe.prices.list({ limit: 100 });
+}
+
+app.get("/vinyls", async (REQUEST, REZ) => {
+    try {
+      const response = await getProducts();
+      const active = response.data.filter(x => x.active == true);
+      let tgt = active.map((album) => {
+        return {
+          id: album.id,
+          name: album.name,
+          priceId: album.default_price,
+          image: album.images[0],
+          metadata: album.metadata,
+        };
+      });
+  
+      const ultimateResult = await Promise.all(
+        tgt.map(async (album) => {
+          if (album.priceId != null) {
+            let newPrice = await getPrice(album.priceId);
+            return {
+              ...album,
+              convertedPrice: newPrice.unit_amount,
+              currency: newPrice.currency
+            };
+          }
+          return { ...album };
+        })
+      );
+  
+      REZ.send(ultimateResult);
+      console.log(ultimateResult);
+    } catch (error) {
+      console.error(error);
+      REZ.sendStatus(500);
+    }
+  });
+
+app.get("/vinylsDB", (req, res) => {
+  //   const params = {
+  //     TableName: "vinyls",
+  //   };
+  //   database.scan(params, (err, data) => {
+  //     if (err) {
+  //       console.log(err);
+  //       res.send(err);
+  //     } else {
+  //       // console.log(data);
+  //       res.send(transformArray(data["Items"]));
+  //     }
+  //   });
 });
 
-app.get('/vinylsDB', (req, res) => {
-    const params = {
-        TableName: 'vinyls'
-    };
+app.post("/checkout", async (req, res) => {
+  const cart = req.body.cart;
+  console.log(
+    cart.map((item) => {
+      return { price: item.stripe_priceid, quantity: item.quantity };
+    })
+  );
 
-    database.scan(params, (err, data) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: cart.map((item) => {
+      return { price: item.stripe_priceid, quantity: item.quantity };
+    }),
+    mode: "payment",
+    success_url: `${DOMAIN}?success=true`,
+    cancel_url: `${DOMAIN}?canceled=true`,
+  });
+
+  res.send(session.url);
+});
+
+app.post("/deleteVinyl", async (req, res) => {
+  const strAPI = require("stripe")(process.env.STRIPE_SEC);
+  const id = req.body.id;
+
+  await strAPI.products
+    .update(id, { default_price: null, active: false })
+    .then((result) => {
+      console.log("success creation", result);
+      var params = {
+        TableName: "vinyls",
+        Key: {
+          id: { S: id },
+        },
+      };
+
+      database.deleteItem(params, (err, data) => {
         if (err) {
-            console.log(err);
-            res.send(err);
+          console.log("something went wrong from deleting in db", err);
         } else {
-            // console.log(data);
-            res.send(transformArray(data['Items']));
+          console.log("deletion was successful", data);
+          res.send("deletion was successful.");
         }
+      });
+    })
+    .catch((err) => {
+      console.log("error", err);
     });
 });
 
+app.put("/createVinyl", async (req, res) => {
+  const strAPI = require("stripe")(process.env.STRIPE_SEC);
 
-function checkStripe() {
-        var strAPI = require("stripe")(process.env.STRIPE_SEC);
-
-        // Retrieve all products
-        strAPI.products.list(
-        { limit: 100 },
-        function(err, products) {
-            // Handle any errors
-            if (err) {
-            console.log(err);
-            return;
-            }
-
-            // Do something with the products
-            console.log(products);
-        }
-        );
-}
-
-// checkStripe();
-
-app.post('/checkout', async (req, res) => {
-    const cart = req.body.cart;
-    console.log(cart.map(item => {return { price: item.stripe_priceid, quantity: item.quantity }}));
-
-    const session = await stripe.checkout.sessions.create({
-        line_items: cart.map(item => {return { price: item.stripe_priceid, quantity: item.quantity }}),
-        mode: 'payment',
-        success_url: `${DOMAIN}?success=true`,
-        cancel_url: `${DOMAIN}?canceled=true`,
-      });
-      
-
-    res.send(session.url);
-});
-
-
-app.post('/deleteVinyl', async (req, res) => {
-    const strAPI = require("stripe")(process.env.STRIPE_SEC);
-    const id = req.body.id;
-
-    await strAPI.products.update(
-        id,
-        {default_price: null, active: false}
-      )
-    .then(result => { console.log('success creation', result)
-        var params = {
-            TableName: 'vinyls',
-            Key: {
-                'id': { "S": id }
-            }
-        };
-
-        database.deleteItem(params, (err, data) => {
-            if (err) { console.log('something went wrong from deleting in db', err) }
-            else { console.log('deletion was successful', data); res.send('deletion was successful.') }
-        });
-        
+  await strAPI.products
+    .create({
+      name: `${req.body.album} by ${req.body.artist}`,
+      default_price_data: {
+        unit_amount: req.body.price * 100,
+        currency: "usd",
+      },
+      images: [req.body.coverImg],
+      metadata: {
+        artist: req.body.artist,
+        album: req.body.album,
+        year: req.body.year,
+        genre: req.body.genre,
+      },
     })
-    .catch(err => { console.log('error', err) })
+    .then((result) => {
+      console.log(
+        `${req.body.album} by ${req.body.artist} added to Stripe! Adding to database now...`
+      );
+      const params = {
+        TableName: "vinyls",
+        Item: {
+          id: { S: result.id },
+          artist: { S: req.body.artist },
+          album: { S: req.body.album },
+          year: { S: `${req.body.year}` },
+          genre: { S: req.body.genre },
+          inStock: { N: `${req.body.inStock}` },
+          coverImg: { S: req.body.coverImg },
+          price: { N: `${req.body.price}` },
+          quantity: { N: `0` },
+          stripe_prodid: { S: result.id },
+          stripe_priceid: { S: result.default_price },
+        },
+      };
+      // will need result.id for stripe_prodid and result.default_price for stripe_priceid
 
-});
-
-app.put('/createVinyl', async (req, res) => {
-    const strAPI = require("stripe")(process.env.STRIPE_SEC);
-
-    await strAPI.products.create({
-        name: `${req.body.album} by ${req.body.artist}`,
-        default_price_data: { unit_amount: (req.body.price * 100), currency: 'usd' },
-        images: [req.body.coverImg],
-        metadata: {
-            'artist': req.body.artist,
-            'album': req.body.album,
-            'year': req.body.year,
-            'genre': req.body.genre,
+      database.putItem(params, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.send(err);
+        } else {
+          console.log(data);
+          console.log(
+            `After adding to Stripe and Database, ${req.body.album} by ${req.body.artist} has been added successfully!`
+          );
+          res.send(data);
         }
-    }).then((result) => {
-        console.log(`${req.body.album} by ${req.body.artist} added to Stripe! Adding to database now...`);
-        const params = {
-            TableName: 'vinyls',
-            Item: {
-                'id': {"S": result.id},
-                'artist': {"S": req.body.artist},
-                'album': {"S": req.body.album},
-                'year': {"S": `${req.body.year}`},
-                'genre': {"S": req.body.genre},
-                'inStock': {"N": `${req.body.inStock}`},
-                'coverImg': {"S": req.body.coverImg},
-                'price': {"N": `${req.body.price}`},
-                'quantity': {"N" : `0`},
-                'stripe_prodid': {"S": result.id },
-                'stripe_priceid': {"S": result.default_price}
-            }
-        };
-        // will need result.id for stripe_prodid and result.default_price for stripe_priceid
-
-        database.putItem(params, (err, data) => {
-            if (err) {
-                console.log(err);
-                res.send(err);
-            } else {
-                console.log(data);
-                console.log(`After adding to Stripe and Database, ${req.body.album} by ${req.body.artist} has been added successfully!`)
-                res.send(data);
-            }
-        });
-    }).catch((err) => {
-        console.log(err)
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
 });
 
 app.listen(port, () => {
-    console.log(`server deployed on port ${port}`)
+  console.log(`server deployed on port ${port}`);
 });
-
