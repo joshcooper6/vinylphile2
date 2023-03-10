e8922da87f694c53509f685cacc4417c8be8173f
Joshua Cooper <65969149+joshcooper6@users.noreply.github.com>
2023-03-09 19:14:47 -0800

    modal added

--- front/src/components/ActiveAlbumModal.jsx
+++ front/src/components/ActiveAlbumModal.jsx
@@ -1,8 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import AlbumContext from "../AlbumContext";
+import formatPrice from "../funcs/formatPrice";
 
 function ActiveAlbumModal(props) {
   const { activeAlbum, setActiveAlbum, addToCart } = useContext(AlbumContext);
+  const [showTracks, setShowTracks] = useState(false);
 
   return (
     <div
@@ -12,12 +14,36 @@ function ActiveAlbumModal(props) {
           : "hidden"
       }
     >
-      <div className="w-10/12 p-4 rounded-xl flex-col items-center justify-center flex max-w-[400px] bg-blue-100 z-[100]">
-        <button
-          children={`Add To Cart`}
-          className={`w-full bg-blue-900 tracking-tight text-blue-100 max-w-[300px] p-4 rounded-full`}
-          onClick={() => addToCart(activeAlbum)}
-        />
+      <div className="w-10/12 p-6 gap-4 rounded-xl flex-col items-center justify-center flex max-w-[400px] bg-blue-100 z-[100]">
+        <div className="flex flex-col justify-center items-center gap-4">
+          <img className="rounded-xl contrast-125" src={activeAlbum?.image} />
+          <div className="self-start">
+            <h2
+              children={activeAlbum?.metadata.album}
+              className={"text-xl font-bold"}
+            />
+            <h3 children={activeAlbum?.metadata.artist} className={"text-md"} />
+          </div>
+        </div>
+
+        <div className="flex flex-col w-full justify-center items-center gap-2">
+          <button
+            children={showTracks ? "Hide Tracks" : "Show Tracks"}
+            className={`w-full bg-blue-900 tracking-tight text-blue-100 max-w-[300px] p-4 rounded-full`}
+            onClick={() => setShowTracks((prev) => !prev)}
+          />
+          <button
+            children={`Add To Cart (${formatPrice(
+              activeAlbum?.convertedPrice,
+              activeAlbum?.currency
+            )})`}
+            className={`w-full bg-blue-900 tracking-tight text-blue-100 max-w-[300px] p-4 rounded-full`}
+            onClick={() => {
+              addToCart(activeAlbum);
+              setActiveAlbum(null);
+            }}
+          />
+        </div>
       </div>
       <button
         children={"Dismiss"}
